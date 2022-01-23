d3ce62ffe0cbdfb717bdfc337f2828a72f2eda85
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-01-23 15:01:15 +0530

    added new test cases

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,23 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+pair_number_color_mapping =[]
 
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    pair_number_color_mapping = get_pair_number_color_mapping()
+    for pair in pair_number_color_mapping:
+        print(f'{pair[0]} | {pair[1]} | {pair[2]}')
+    return len(major_colors) * len(minor_colors) , pair_number_color_mapping
+
+def get_pair_number_color_mapping():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            pair_number_color_mapping.append([i * 5 + j, major, minor])
+    return pair_number_color_mapping
 
 
-result = print_color_map()
+result, pair_number_color_mapping = print_color_map()
 assert(result == 25)
+assert(1 in pair_number_color_mapping[0])
+assert(len(major_colors)*len(minor_colors) in pair_number_color_mapping[-1])
+
 print("All is well (maybe!)\n")
